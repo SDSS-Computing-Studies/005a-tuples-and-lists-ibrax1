0b3f247b0c309e7bd5abb17a43c6b426c5434789
ibrax1 <ibrahima253@deltalearns.ca>
2021-02-23 09:27:17 -0800

    push

--- task1.py
+++ task1.py
@@ -24,4 +24,8 @@ The animal at that index is Dog
 
 
 
-animals =
+animals = ["Cat","Fish","Dog","Bear","Turtle"]
+animals.sort()
+print(animals)
+q = input= ("Enter the index for an animal:")
+animals.index(q)
